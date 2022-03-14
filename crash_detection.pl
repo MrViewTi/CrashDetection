6f258169eb7e1063df45e8a3e1f82f5ea349027d
MrViewTi <arsen0903@hotmail.com>
2022-03-14 16:54:25 +0100

    fixed

--- crash_detection.pl
+++ crash_detection.pl
@@ -1,4 +1,4 @@
 nn(dangerdist_cnn, [X]) :: car_obstruction_distance(X).
-nn(dangerordr_cnn, [X]) :: car_obstruction_order(X)
+nn(dangerordr_cnn, [X]) :: car_obstruction_order(X).
 
 crash(X) :- car_obstruction_distance(X), car_obstruction_order(X).
